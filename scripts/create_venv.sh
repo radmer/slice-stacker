8b057f9cee49471c977d60c21b27c60068bb1da6
Randall J. Radmer <radmer@gmail.com>
2026-04-14 19:33:30 -0700

    Fix script that creates a venv

--- scripts/create_venv.sh
+++ scripts/create_venv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-venv_name=omsystems
+venv_name=slice-stacker
 venv_dir="$HOME/VENV"
 
 echo "Create python venv: $venv_name"
@@ -17,23 +17,7 @@ python3 -m venv $venv_name --prompt=$venv_name
 source $venv_name/bin/activate
 pip install --upgrade pip
 pip install --upgrade numpy
-pip install --upgrade scipy
-pip install --upgrade exifread
+pip install --upgrade opencv-python
+pip install --upgrade tifffile
 
-#pip install --upgrade pillow
-#pip install --upgrade prequests
-#pip install --upgrade olympuswifi
-
-#pip install --upgrade matplotlib
-#pip install --upgrade scipy
-#pip install --upgrade nibabel
-#pip install --upgrade scikit-image
-#pip install --upgrade numpy-stl
-#pip install --upgrade trimesh
-#pip install --upgrade "napari[all]"
-#
-#pip install --upgrade pyvista
-#pip install --upgrade sympy
-#pip install --upgrade pymeshlab
-#pip install --upgrade polyscope
 deactivate
